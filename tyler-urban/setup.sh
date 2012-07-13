8e6ab21058b130b2097ff441bd93d6721ba2d910
Christopher Groskopf <staringmonkey@gmail.com>
2012-07-12 21:06:14 -0500

    Setup cleanup.

--- tyler-urban/setup.sh
+++ tyler-urban/setup.sh
@@ -7,6 +7,8 @@ USER="onyxfish"
 OGR_PG="host=$HOST dbname=$DATABASE user=$USER"
 SRID="900913"
 
+PSQL="psql -h $HOST -q $DATABASE -U $USER"
+
 ogr2ogr -f "PostgreSQL" PG:"$OGR_PG" -nln sidewalks sidewalk -overwrite -nlt multilinestring -t_srs EPSG:$SRID
 ogr2ogr -f "PostgreSQL" PG:"$OGR_PG" -nln bike_routes bike_routes -overwrite -nlt multilinestring -t_srs EPSG:$SRID
 ogr2ogr -f "PostgreSQL" PG:"$OGR_PG" -nln structures structures -overwrite -nlt multilinestring -t_srs EPSG:$SRID
@@ -18,9 +20,11 @@ ogr2ogr -f "PostgreSQL" PG:"$OGR_PG" -nln schools schools -overwrite -nlt multip
 ogr2ogr -f "PostgreSQL" PG:"$OGR_PG" -nln interact parks -overwrite -nlt geometry -t_srs EPSG:$SRID -sql "select NAME, 'park' as TYPE from Parks"
 ogr2ogr -f "PostgreSQL" PG:"$OGR_PG" -nln interact schools -append -nlt geometry -t_srs EPSG:$SRID -sql "select FACILITY as NAME, 'school' as TYPE from Educational"
 
-echo "CREATE TABLE centerline_merged AS SELECT ST_LineMerge(ST_Union(wkb_geometry)) as wkb_geometry, fullname FROM centerline GROUP BY fullname;" | psql -h $HOST -q hacktyler-urban -U onyxfish
+$PSQL -c "CREATE TABLE centerline_merged AS SELECT ST_LineMerge(ST_Union(wkb_geometry)) as wkb_geometry, fullname FROM centerline GROUP BY fullname;"
 
-echo "CREATE TABLE park_centroids AS SELECT ST_Centroid(ST_Union(wkb_geometry)) as wkb_geometry, name FROM parks GROUP BY name" | psql -h $HOST -q hacktyler-urban -U onyxfish
+$PSQL -c "CREATE TABLE park_centroids AS SELECT ST_Centroid(ST_Union(wkb_geometry)) as wkb_geometry, name || ' PARK' as name FROM parks GROUP BY name;"
 
-echo "CREATE TABLE school_centroids AS SELECT ST_Centroid(ST_Union(wkb_geometry)) as wkb_geometry, facility as name FROM schools GROUP BY facility" | psql -h $HOST -q hacktyler-urban -U onyxfish
+$PSQL -c "UPDATE park_centroids SET name='CHILDRENS PARK' WHERE name='CHILDREN PARK';"
+
+$PSQL -c "CREATE TABLE school_centroids AS SELECT ST_Centroid(ST_Union(wkb_geometry)) as wkb_geometry, facility as name FROM schools GROUP BY facility"
 
